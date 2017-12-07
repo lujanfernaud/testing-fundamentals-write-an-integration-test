b72379b3cb73c111e775a83db0c7cbd5be593af7
lujanfernaud <lujanfernaud@gmail.com>
2017-12-07 15:49:28 +0000

    Implement user_creates_person_spec

--- spec/features/user_creates_person_spec.rb
+++ spec/features/user_creates_person_spec.rb
@@ -2,6 +2,22 @@ require "rails_helper"
 
 feature "user creates person" do
   scenario "with valid data" do
-    pending "Use Capybara to create a new person"
+    visit new_person_path
+
+    fill_in "First name", with: "Dong"
+
+    click_on "Create Person"
+
+    expect(page).to have_content("Person created")
+  end
+
+  scenario "with invalid data" do
+    visit new_person_path
+
+    fill_in "First name", with: ""
+
+    click_on "Create Person"
+
+    expect(page).to have_content("First name can't be blank")
   end
 end
